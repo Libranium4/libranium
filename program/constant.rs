7807557e056b896d5b937f7c652f9f3a09658fd9
Libranium4 <126936425+Libranium4@users.noreply.github.com>
2023-03-04 16:42:14 +0700

    update todo status

--- program/constant.rs
+++ program/constant.rs
@@ -10,10 +10,10 @@ pub const TODO_TAG: &[u8] = b"TODO_STATE";
 pub const TODO_STATUS_TODO: u8 = 0;
 
 #[constant]
-pub const TODO_STATUS_START: u8 = 0;
+pub const TODO_STATUS_START: u8 = 1;
 
 #[constant]
-pub const TODO_STATUS_DONE: u8 = 0;
+pub const TODO_STATUS_DONE: u8 = 2;
 
 #[constant]
-pub const TODO_STATUS_ABANDON: u8 = 0;
+pub const TODO_STATUS_ABANDON: u8 = 3;
